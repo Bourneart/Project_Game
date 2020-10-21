80829ceec1912aa5e35535534187ade8ebe45e23
aks <cf>
2020-10-21 22:09:48 +0300

    Класс и интерфейс NPC (неигровые персонажи)

---
+++ Assets/Scripts/IMobCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public interface IMobCard
+{
+    string MobID { get; }
+    string MobName { get; }
+
+    int MobReflex { get; set; }
+    int MobBody { get; set; }
+    int MobIntrllegence { get; set; }
+    int MobWisdom { get; set; }
+    float MobHitPoints { get; set; }
+    float MobManaPoints { get; set; }
+    float MobMagicArmorClass { get; set; }
+}
---
+++ Assets/Scripts/MobCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class MobCard : IMobCard
+{
+    string IMobCard.MobID => _mobID;
+
+    string IMobCard.MobName => _mobName;
+
+    int IMobCard.MobReflex { get => _mobReflex; set => throw new NotImplementedException(); }
+    int IMobCard.MobBody { get => _mobBody; set => throw new NotImplementedException(); }
+    int IMobCard.MobIntrllegence { get => _mobIntellegence; set => throw new NotImplementedException(); }
+    int IMobCard.MobWisdom { get => _mobWisdom; set => throw new NotImplementedException(); }
+    float IMobCard.MobHitPoints { get => _mobHitPoints; set => throw new NotImplementedException(); }
+    float IMobCard.MobManaPoints { get => _mobManaPoints; set => throw new NotImplementedException(); }
+    float IMobCard.MobMagicArmorClass { get => _mobMagicArmorClass; set => throw new NotImplementedException(); }
+
+    private readonly string _mobID;
+
+    internal string _mobName;
+
+    protected int _mobReflex;
+    protected int _mobBody;
+    protected int _mobIntellegence;
+    protected int _mobWisdom;
+    protected float _mobHitPoints;
+    protected float _mobManaPoints;
+    protected float _mobMagicArmorClass;
+}
