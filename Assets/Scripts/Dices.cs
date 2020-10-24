0f749da2091b5a740fd5d3fa2261391c896c6223
aks <cf>
2020-10-24 16:36:04 +0300

    Функции расчёта действий, атаки и урона

---
+++ Assets/Scripts/Dices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+public class Dices
+{
+    public static int WhiteDice(uint seed)
+    {
+        Unity.Mathematics.Random WhiteDice = new Unity.Mathematics.Random();
+        return WhiteDice.NextInt();
+    }
+
+    public static float WeaponDice(WeaponCard weapon)
+    {
+        Unity.Mathematics.Random WeaponDice = new Unity.Mathematics.Random();
+        return WeaponDice.NextFloat(weapon.MinD, weapon.MaxD);
+    }
+
+    public static double GoldDice(IWeaponCard iweaponcard)
+    {
+        Unity.Mathematics.Random GoldDice = new Unity.Mathematics.Random();
+        return GoldDice.NextDouble(iweaponcard.MinimalDamage, iweaponcard.MaximalDamage);
+    }
+}
+  
+
