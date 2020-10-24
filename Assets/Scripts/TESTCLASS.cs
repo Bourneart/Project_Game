e4a685f21ffb58422e20e9c650c248f8be0708df
aks <cf>
2020-10-24 16:36:58 +0300

    Внимание!!! Тестовые файлы

---
+++ Assets/Scripts/TESTCLASS.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+public class TESTCLASS
+{
+    private float[] _fireWeaponData = new float[5];
+
+    public float[] TakeData(string id)
+    {
+        switch (id)
+        {
+            case "wfn100":
+                _fireWeaponData[0] = 1.2f;
+                _fireWeaponData[1] = 1.2f;
+                _fireWeaponData[2] = 1.2f;
+                _fireWeaponData[3] = 1.2f;
+                _fireWeaponData[4] = 1.2f;
+                return _fireWeaponData;
+
+            case "wfn200":
+                _fireWeaponData[0] = 1.2f;
+                _fireWeaponData[1] = 1.2f;
+                _fireWeaponData[2] = 1.2f;
+                _fireWeaponData[3] = 1.2f;
+                _fireWeaponData[4] = 1.2f;
+                return _fireWeaponData;
+
+            case "wfn300":
+                _fireWeaponData[0] = 1.2f;
+                _fireWeaponData[1] = 1.2f;
+                _fireWeaponData[2] = 1.2f;
+                _fireWeaponData[3] = 1.2f;
+                _fireWeaponData[4] = 1.2f;
+                return _fireWeaponData;
+
+            case "wfn400":
+                _fireWeaponData[0] = 1.2f;
+                _fireWeaponData[1] = 1.2f;
+                _fireWeaponData[2] = 1.2f;
+                _fireWeaponData[3] = 1.2f;
+                _fireWeaponData[4] = 1.2f;
+                return _fireWeaponData;
+
+            case "wfn500":
+                _fireWeaponData[0] = 1.2f;
+                _fireWeaponData[1] = 1.2f;
+                _fireWeaponData[2] = 1.2f;
+                _fireWeaponData[3] = 1.2f;
+                _fireWeaponData[4] = 1.2f;
+                return _fireWeaponData;
+
+            case "wfn600":
+                _fireWeaponData[0] = 1.2f;
+                _fireWeaponData[1] = 1.2f;
+                _fireWeaponData[2] = 1.2f;
+                _fireWeaponData[3] = 1.2f;
+                _fireWeaponData[4] = 1.2f;
+                return _fireWeaponData;
+
+            case "wfn700":
+                _fireWeaponData[0] = 1.2f;
+                _fireWeaponData[1] = 1.2f;
+                _fireWeaponData[2] = 1.2f;
+                _fireWeaponData[3] = 1.2f;
+                _fireWeaponData[4] = 1.2f;
+                return _fireWeaponData;
+
+            case "wfn800":
+                _fireWeaponData[0] = 1.2f;
+                _fireWeaponData[1] = 1.2f;
+                _fireWeaponData[2] = 1.2f;
+                _fireWeaponData[3] = 1.2f;
+                _fireWeaponData[4] = 1.2f;
+                return _fireWeaponData;
+        }
+    }
+}
