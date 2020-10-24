142e44789851d497b2a54f2b3c55269531bc0b68
aks <cf>
2020-10-24 16:36:25 +0300

    Интерфейс и карта оружия

---
+++ Assets/Scripts/Weapon/IWeapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public interface IWeapon
+{
+    /*
+     *    Weapon ID форматирование:
+     *    строка представляет собой
+     *    набор из трёх буквенных
+     *    и трёх цифровых символов
+     *    
+     *    Пример:
+     *    string IWeapon.WeaponID => "wfn100";
+     *    где первый символ 
+     *    (w) - weapon (маркировака "оружие")
+     *    (f) - fire (принадлежность к стихии)
+     *    (n) - запасной символ (для узких корректировок)
+     */
+
+    string WeaponID { get; }
+    string WeaponName { get; }
+
+    float MinD { get; }
+    float MaxD { get; }
+    float FirstGainFactor { get; }
+    float SecondGainFactor { get; }
+    float ThirdGainFactor { get; }
+
+    void InputData(WeaponTable table);
+    
+
+    float Damage(WeaponCard weapon, HeroCard hero);
+
+}
---
+++ Assets/Scripts/Weapon/WeaponCard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class WeaponCard :  IWeapon
+{
+    public delegate float Dice(WeaponCard weapon);
+    Dice dice;
+
+    string IWeapon.WeaponID => _weaponID;
+
+    string IWeapon.WeaponName => _weaponName;
+
+    public float MinD => _minD;
+    public float MaxD => _maxD;
+    public float FirstGainFactor => _fgf;
+    public float SecondGainFactor => _sgf;
+    public float ThirdGainFactor => _tgf;
+
+    private string _weaponID;
+    public string _weaponName;
+
+    protected float _minD;
+    protected float _maxD;
+    protected float _fgf;
+    protected float _sgf;
+    protected float _tgf;
+
+    public void InputData(WeaponTable table)
+    {
+        throw new NotImplementedException();
+    }
+
+    public float Damage(WeaponCard weaponCard, HeroCard hero)
+    {
+        dice = Dices.WeaponDice;
+        return dice(weaponCard) + hero._intellegence * weaponCard._fgf;
+    }
+
+}
